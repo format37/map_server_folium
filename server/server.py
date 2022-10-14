444947c6794873053bf56dab3bcb38d450b05a46
alex <format37@gmail.com>
2022-10-14 16:33:36 +0400

    modified:   server/server.py

--- server/server.py
+++ server/server.py
@@ -4,6 +4,14 @@ import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
 from io import StringIO
+import logging
+import numpy as np
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 async def call_test(request):
 	content = "get ok"
@@ -24,14 +32,24 @@ def map_generator(df,zoom_start=10):
 			color=row['color'], 
 			fill_opacity = row['fill_opacity']
 		).add_to(marker_cluster)
+	# draw a line between the points	
+	for i in range(len(df)-1):
+		folium.PolyLine(
+			[[df.iloc[i].lat,df.iloc[i].lon],[df.iloc[i+1].lat,df.iloc[i+1].lon]],
+			weight=5,
+			color='blue',
+			opacity=0.5
+		).add_to(map)
 	return map._repr_html_()
 
 
 async def call_map(request):
-	
+	logger.info("call_map")
 	csv_text = str(await request.text()).replace('\ufeff', '')
 	df = pd.read_csv(StringIO(csv_text), sep=';')
+	logger.info('df len: '+str(len(df)))
 	response = map_generator(df)
+	logger.info("response len: "+str(len(response)))
 	return web.Response(text=str(response),content_type="text/html")
 
 app = web.Application(client_max_size=1024**3)
